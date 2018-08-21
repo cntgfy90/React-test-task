3f68aafffd641d8aea0a610b52ec3957288e7e57
cntgfy90 <stepan.kolomiyets@gizzaro.io>
2018-08-21 16:08:08 +0300

    Install React-router-dom and Login/Calendar paged draft

---
+++ src/components/pages/CalendarPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CalendarPage extends React.Component {
+  render() {
+    return (
+      <div>
+        Calendar Page
+      </div>
+    );
+  }
+}
+
+export defualt CalendarPage;

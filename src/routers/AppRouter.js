64953630402da9c1f47405414e1a6b465bdf9db1
cntgfy90 <stepan.kolomiyets@gizzaro.io>
2018-08-21 16:11:01 +0300

    Sets AppRouter

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Pages components
+import LoginPage from '../components/pages/LoginPage';
+import CalendarPage from '../components/pages/CalendarPage';
+
+const AppRouter = (props) => (
+  <Router>
+    <Switch>
+      <Route path="/login" component={LoginPage} exact={true} />
+      <Route path="/calendar" component={CalendarPage} exact={true} />
+    </Switch>
+  </Router>
+);
+
+export defualt AppRouter;

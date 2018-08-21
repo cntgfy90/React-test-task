f6b7e9ef684360dfe6f234cd45305c24abe2666c
cntgfy90 <stepan.kolomiyets@gizzaro.io>
2018-08-21 16:15:44 +0300

    Sets bootstrap 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
+// import bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <AppRouter />,
